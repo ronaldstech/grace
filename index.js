f77e90db6ca63920061b25e9f4eed034f068f306
ronaldstech <liwewejacob265@gmail.com>
2026-05-12 17:15:26 +0200

    implemented sessions

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const express = require('express');
+const session = require('express-session');
 
 const app = express();
 
 //middlewares implementation
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(
+    session({
+        secret: '1234',
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+        maxAge: 1000 * 60 * 60 // 1 hour
+        }
+    })
+);
 
 //route implementation
 app.get('/', (req, res) => {
-    res.send('<h1>Hello world</h1>');
+    const user = {
+        username: "grace",
+        age: 20
+    };
+    req.session.username = user;
+    res.send('session saved');
 });
 
 app.get('/login', (req, res) => {
@@ -16,10 +32,11 @@ app.get('/login', (req, res) => {
 })
 
 app.get('/user/profile', async (req, res) => {
+    const username = req.session.username;
     const response = await fetch("http://localhost:4000/api/products");
     const products = await response.json();
     //console.log(posts);
-    res.render('profile', { products });
+    res.render('profile', { products, username });
 })
 
 
